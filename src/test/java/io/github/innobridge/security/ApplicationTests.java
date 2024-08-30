1a104fcc9fc0f2efe60e303819f0ac9980371871
InnoBridge <innobridgetechnology@gmail.com>
2024-08-30 03:08:01 +0000

    first commit

---
+++ src/test/java/io/github/innobridge/security/ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.innobridge.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
